836b5b84d4fccbb740331b7f2fcc6d04383147d2
hhp3075 <hhp3075@maum.ai>
2026-01-16 17:12:57 +0900

    Feat: dice app

---
+++ databinding_ex/app/src/main/java/com/bokchi/databinding_ex/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.bokchi.databinding_ex
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.bokchi.databinding_ex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        val btn = findViewById<Button>(R.id.testBtnId)
+//        btn.setOnClickListener {
+//            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+//        }
+
+        binding.testBtnId.setOnClickListener {
+            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+        }
+
+    }
+}
